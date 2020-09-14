b1d53e96c39a234ae9a855b660662e2692e63c73
jose-jjunior <jose.junior@mercadolivre.com>
2020-09-14 18:31:41 -0300

    eurekaServer starting

--- src/main/java/com/locar/goals/discovery/config/SecurityConfig.java
+++ src/main/java/com/locar/goals/discovery/config/SecurityConfig.java
@@ -1,30 +1,27 @@
-package com.locar.goals.discovery.config;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .csrf()
-            .ignoringAntMatchers("/eureka/**");
-
-        super.configure(http);
-    }
-
-    /*
-    * use to change username and password
-    */
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("user")
-//                .password("1234")
-//                .authorities("admin");
+//package com.locar.goals.discovery.config;
 //
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        super.configure(http);
 //    }
-}
+//
+//    /*
+//    * use to change username and password
+//    */
+////    @Override
+////    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+////        auth.inMemoryAuthentication()
+////                .withUser("user")
+////                .password("1234")
+////                .authorities("admin");
+////
+////    }
+//}
